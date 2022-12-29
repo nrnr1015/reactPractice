5a6d8dc0622bb5649e6f37ee4d5c73321002d015
nrnr1015 <llr2916@naver.com>
2022-12-29 10:31:10 +0900

    commit

---
+++ blog/src/App.js
@@ -0,0 +1,87 @@
+/*eslint-disable*/
+
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function App() {
+  // 서버에서 가져온다고 가정.
+  let post = '강남 우동 맛집';
+
+  // useState = b:state 변경을 도와주는 함수 
+  // 자주 변경이 될 것 같은 data를 useState
+  let [array , setArray] = useState(['하마 코트 추천', '여자 코트 추천', '아동 코트 추천'])
+  let [like, setLike] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+
+  function abcSort(){
+    let arrayCopy = [...array].sort();
+    setArray(arrayCopy)
+  }
+  
+  
+  return (
+    <div className="App">
+      <div className="block-nav">
+        <h1 style={{color : 'red', fontSize: '16px'}}>logo</h1>
+      </div>
+      
+      <button onClick={abcSort}>가나다 정렬</button>
+
+      <button onClick={ () => {
+        // [...] 괄호 벗기고, 다시 씌워줘 deep copy
+        let copy = [...array];
+        copy[0] = '따듯한 코트 추천'
+        setArray(copy); // 새로운 state 생성
+        }}>글수정</button>
+
+      <div className="list">
+        {
+          array.map(function (item, i){
+            return (
+              <div>
+                <h4 item={item} i={i} key={i}>
+                  {item}
+                  <span onClick={() => {
+                    let likeCopy = [...like]
+                    likeCopy[i] = likeCopy[i] + 1;
+                    setLike(likeCopy)
+                  }}>🍎 <span>{like[i]}</span></span>
+                </h4>
+                <p>날짜</p>
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <button onClick={() => {
+        setModal(!modal)
+      }}>
+        Modal open
+      </button>
+
+      {
+        modal === true ? <Modal /> : null
+      }
+      
+    </div>
+  );
+}
+
+// component는 언제 쓰는가?
+// 1. 반복적일때 2. 큰 페이지를 쓸때 3. 지주 변경되는 UI
+// state 가져다 쓸때 주의 할 것.
+// const로 함수 만들면 실수 방지하기 좋음. const Modal = () => {}
+const Modal = () => {  
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+export default App;
