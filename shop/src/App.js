52ddb00d00af855cb2c7f9c3734e4dcd13779b5e
nrnr1015 <llr2916@naver.com>
2023-01-02 22:43:52 +0900

    반복문 컴포넌트로 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -30,13 +30,29 @@ function App(props) {
 
         <div className="container">
           <div className="row">
-            {itemList}
+            {/* {itemList} */}
+            <ItemListCard shoes={shoes}></ItemListCard>
           </div>
         </div>
     </div>
   );
 }
 
+//{itemList} 컴포넌트로 만들기 && props 전송
+const ItemListCard = (props) => {
+  return(
+    props.shoes.map( (item, i) => {
+      return(
+        <div className={`col-md-4 item-elem item_${i}`} id={`item-${i}`} key={i}>
+          <img src={item.img} width="80%" alt={item.title}/>
+          <h4>{props.shoes[item.title]}</h4>
+          <p>{item.content}</p>
+          <p>{item.price}</p>
+        </div>
+      )
+    })
+  )
+}
 
 
 export default App;
