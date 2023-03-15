383e746a6164ee0e6e5c191ebd99fa3f8809ceaf
nrnr1015 <llr2916@gentlemonster.com>
2023-03-15 18:07:07 +0900

    package 추가

--- lotto-data/src/App.js
+++ lotto-data/src/App.js
@@ -1,15 +1,15 @@
-
+import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 import LottoData from './LottoData.js';
 
 
 function App() {
   return (
-    <div className="my">
+    <div className="App">
+      <header className="App-header"></header>
       <LottoData></LottoData>
     </div>
   );
