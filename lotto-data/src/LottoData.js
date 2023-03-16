9b1bb6392bd3e5299c120f8f940b96209ee0b53c
nrnr1015 <llr2916@gentlemonster.com>
2023-03-16 09:24:33 +0900

    proxy 추가

--- lotto-data/src/LottoData.js
+++ lotto-data/src/LottoData.js
@@ -4,52 +4,30 @@ import axios from "axios";
 
 function LottoData() {
   const [lottoData, setLottoData] = useState([]);
-
+  const BASEURL = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+1'
   useEffect(() => {
     async function getLottoData() {
       const data = [];
-      const delayTime = 500; // API 요청 간격 (500ms 이상 권장)
-
-      for (let i = 1; i <= 999; i++) { // 1회 ~ 999회
-        await delay(delayTime); // 요청 간격을 위한 delay
-
-        try {
-          const response = await axios.get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${i}`);
-          data.push(response.data);
-        } catch (error) {
-          console.error(`Error while fetching data for round ${i}: ${error}`);
-        }
+      try {
+        const response = await axios.get('/users',{
+          //항목에 새롭게 넣을 데이터
+          username: "stone",
+          password: "123"
+        });
+        console.log('response: ', response);
+        data.push(response.data);
+      } catch (error) {
+        console.error(`Error while fetching data for round `);
       }
 
-      let i = 1000; // 1000회부터의 데이터는 while 문을 사용하여 처리
-      while (true) {
-        await delay(delayTime); // 요청 간격을 위한 delay
-
-        try {
-          const response = await axios.get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${i}`);
-          const dataItem = response.data;
-
-          if (dataItem.returnValue === "fail") {
-            break; // 데이터가 더 이상 없으면 while 루프 종료
-          }
-
-          data.push(dataItem);
-          i++;
-        } catch (error) {
-          console.error(`Error while fetching data for round ${i}: ${error}`);
-          i++; // 에러가 발생하더라도 while 루프는 계속 진행
-        }
-      }
-
+      
       setLottoData(data);
     }
 
     getLottoData();
   }, []);
 
-  function delay(ms) { // 지정한 시간(ms) 만큼 delay 하는 함수
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+
 
   return (
     <div>
@@ -66,14 +44,8 @@ function LottoData() {
           </thead>
           <tbody>
             {lottoData.map(item => (
-              <tr key={item.drwNo}>
-                <td>{item.drwNo}회</td>
-                <td>
-                  {item.drwtNo1} {item.drwtNo2} {item.drwtNo3} {item.drwtNo4} {item.drwtNo5} {item.drwtNo6}
-                </td>
-                <td>{item.bnusNo}</td>
-                <td>{item.firstWinamnt.toLocaleString()}</td>
-                <td>{item.firstPrzwnerCo.toLocaleString()}</td>
+              <tr key={item.name}>
+               
               </tr>
             ))}
           </tbody>
---
+++ lotto-data/src/setupProxy.js
@@ -0,0 +1,11 @@
+// src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/users', {
+      target: 'https://jsonplaceholder.typicode.com',
+      changeOrigin: true,
+    }),
+  );
+};
