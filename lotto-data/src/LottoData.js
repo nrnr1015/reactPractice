180f4c43c2c244e230a9ef1ea1f19073a2dead34
nrnr1015 <llr2916@naver.com>
2023-03-30 22:26:06 +0900

    lotto 데이터 꽂기

--- lotto-data/src/LottoData.js
+++ lotto-data/src/LottoData.js
@@ -1,88 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from "axios";
-
-
-// function LottoData() {
-
-//   const [lottoData, setLottoData] = useState([]);
-//   useEffect(() => {
-//     async function getLottoData() {
-//       const data = [];
-//       const API_URL = '/common.do?method=getLottoNumber&drwNo=';
-
-//       const delayTime = 500; 
-
-//       for (let i = 1; i <= 999; i++) { // 1회 ~ 999회
-//         await delay(delayTime); // 요청 간격을 위한 delay
-
-//         try {
-//           const response = await axios.get(API_URL + `${i}`);
-//           console.log('response: ', response.data);
-//           data.push(response.data);
-//         } catch (error) {
-//           console.error(`Error while fetching data for round ${i}: ${error}`);
-//         }
-//       }
-
-//       let i = 1000; // 1000회부터의 데이터는 while 문을 사용하여 처리
-//       while (true) {
-//         await delay(delayTime); // 요청 간격을 위한 delay
-
-//         try {
-//           const response = await axios.get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${i}`);
-//           const dataItem = response.data;
-
-//           if (dataItem.returnValue === "fail") {
-//             break; // 데이터가 더 이상 없으면 while 루프 종료
-//           }
-
-//           data.push(dataItem);
-//           i++;
-//         } catch (error) {
-//           console.error(`Error while fetching data for round ${i}: ${error}`);
-//           i++; // 에러가 발생하더라도 while 루프는 계속 진행
-//         }
-//       }
-      
-//       setLottoData(data);
-//     }
-//     getLottoData();
-//   }, []);
-//   function delay(ms) { // 지정한 시간(ms) 만큼 delay 하는 함수
-//     return new Promise(resolve => setTimeout(resolve, ms));
-//   }
-//   console.log("lottoData", lottoData.length);
-
-//   return (
-//     <div>
-//       <table>
-//           <thead>
-//             <tr>
-//               <th>회차</th>
-//               <th>당첨번호</th>
-//               <th>보너스번호</th>
-//               <th>1등 당첨금</th>
-//               <th>1등 당첨자 수</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {lottoData.map(item => (
-//               <tr key={item}>
-//                 <td>{item.drwNo}회</td>
-//                 <td>
-//                   {item.drwtNo1} {item.drwtNo2} {item.drwtNo3} {item.drwtNo4} {item.drwtNo5} {item.drwtNo6}
-//                 </td>
-//                 <td>{item.bnusNo}</td>
-//                 <td>{item.firstWinamnt.toLocaleString()}</td>
-//                 <td>{item.firstPrzwnerCo.toLocaleString()}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//     </div>
-//   );
-// }
-
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
@@ -106,11 +21,28 @@ function LottoData() {
     <div className='lottoDate'>
       {lottoData.map((lotto, i) => (
         <div key={i}>
-          <h2>{lotto.draw_no}</h2>
-          <p>{lotto.numbers}</p>
+          <h2>{lotto.draw_no}</h2> 
+          <ul>
+            {
+              lotto.numbers.map((item,index) => {
+                // 9이하는 두자리 수를 만든다
+                const formatNum = item < 10 ? `0${item}` : item;
+                // item 10이하는 
+                const format10 = item <= 10 ? 'text' : '';
+                return <li 
+                        key={index}  
+                        data-num={formatNum}
+                        className={`number${index} ${format10}`}
+                        >
+                          {formatNum}
+                      </li>
+              })
+            }
+            
+          </ul>
           <p>{lotto.bonus_no}</p>
           {/* {console.log(lottoData[i])} */}
-          {console.log("lotto",lotto)}
+          {/* {console.log("lotto",lotto)} */}
           {/* 나머지 코드 */}
         </div>
       ))}
